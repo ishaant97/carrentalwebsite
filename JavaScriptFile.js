9eeb468c8a45aba34ea060150f316e9e190d535a
Ishaant Kumar Singh <114723130+ishaant-97@users.noreply.github.com>
2022-12-12 11:50:40 +0530

    Add files via upload

---
+++ JavaScriptFile.js
@@ -0,0 +1,15 @@
+function carDeliver() {
+    if (document.getElementById("dataField").value=="") {
+        alert("Fill all the Parameters")
+    }
+    else{
+        alert('Your Car will be delivered Soon!!!')
+    }
+    
+}
+
+
+
+function updates() {
+    alert("Thank You!!! For Subscribing we'll update you will our latest services")
+}
